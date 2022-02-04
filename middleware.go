3a4926f4de84d00f1b482e642301b5de33c83870
jjcapellan <soycape@hotmail.com>
2022-02-04 20:50:33 +0100

    Added: replaced Middleware with GetAuthMiddleware

--- middleware.go
+++ middleware.go
@@ -5,18 +5,26 @@ import (
 	"net/http"
 )
 
-// Middleware is a middleware function to use in the server router
+// GetAuthMiddleware returns a middleware function to use in the server router.
 //
-// If auth cookie is not valid, redirects the user to login url.
-//
-// You could make your custom middleware using the function CheckAuthCookie
-func Middleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := CheckAuthCookie(r); err != nil {
-			log.Println("Bad auth cookie")
-			http.Redirect(w, r, conf.loginUrl, http.StatusSeeOther)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+// Returned middleware redirects the user to login url if auth cookie is not valid, or
+// sends status unauthorized (401) if user auth level is lower than required.
+func GetAuthMiddleware(authLevel int) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if err := CheckAuthCookie(r); err != nil {
+				log.Println("Bad auth cookie")
+				http.Redirect(w, r, conf.loginUrl, http.StatusSeeOther)
+				return
+			}
+
+			cookie, _ := r.Cookie("JJCSESID")
+			if authValue := GetUserAuthLevel(cookie.Value); authValue < authLevel {
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Insufficient authorization level"))
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
 }

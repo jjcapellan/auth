c701644b54a24c15d5e4a1823cf3f4068ed2d1e5
jjcapellan <soycape@hotmail.com>
2022-02-05 13:09:18 +0100

    Fix: swap http codes 401 - 403

--- middleware.go
+++ middleware.go
@@ -8,18 +8,18 @@ import (
 //
 // Returned middleware redirects the user to login url if auth cookie is not valid, or
 // sends status unauthorized (401) if user auth level is lower than required.
-func GetAuthMiddleware(authLevel int) func(http.Handler) http.Handler {
+func GetAuthMiddleware(authLevel int, redirectURL string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if err := CheckAuthCookie(r); err != nil {
-				w.WriteHeader(http.StatusForbidden)
+				w.WriteHeader(http.StatusUnauthorized)
 				w.Write([]byte("Forbidden: Not valid or expired credentials"))
 				return
 			}
 
 			cookie, _ := r.Cookie("JJCSESID")
 			if authValue := GetUserAuthLevel(cookie.Value); authValue < authLevel {
-				w.WriteHeader(http.StatusUnauthorized)
+				w.WriteHeader(http.StatusForbidden)
 				w.Write([]byte("Insufficient authorization level"))
 				return
 			}

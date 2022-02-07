bc25cea5d2d99d38e69bc7c5dd4dcf10340ef856
jjcapellan <soycape@hotmail.com>
2022-02-07 13:14:19 +0100

    Feature: notLoggedURL and forbiddenURL implemented in middleware

--- middleware.go
+++ middleware.go
@@ -6,21 +6,33 @@ import (
 
 // GetAuthMiddleware returns a middleware function to use in the server router.
 //
-// Returned middleware redirects the user to login url if auth cookie is not valid, or
-// sends status unauthorized (401) if user auth level is lower than required.
-func GetAuthMiddleware(authLevel int, redirectURL string) func(http.Handler) http.Handler {
+// Returned middleware redirects the user to notLoggedURL if auth cookie is not valid, or
+// redirects to forbiddenURL if user auth level is lower than required. These two URLs may be
+// an empty string, in which case only will be returned a 403 status code.
+func GetAuthMiddleware(authLevel int, notLoggedURL string, forbiddenURL string) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			if err := CheckAuthCookie(r); err != nil {
-				w.WriteHeader(http.StatusForbidden)
-				w.Write([]byte("Forbidden: Not valid or expired credentials"))
+
+				if notLoggedURL != "" {
+					http.Redirect(w, r, notLoggedURL, http.StatusSeeOther)
+				} else {
+					w.WriteHeader(http.StatusForbidden)
+					w.Write([]byte("Forbidden: Not valid or expired credentials"))
+				}
+
 				return
 			}
 
 			cookie, _ := r.Cookie("JJCSESID")
 			if authValue := GetUserAuthLevel(cookie.Value); authValue < authLevel {
-				w.WriteHeader(http.StatusForbidden)
-				w.Write([]byte("Forbidden: Insufficient authorization level"))
+
+				if forbiddenURL != "" {
+					http.Redirect(w, r, forbiddenURL, http.StatusSeeOther)
+				} else {
+					w.WriteHeader(http.StatusForbidden)
+					w.Write([]byte("Forbidden: Insufficient authorization level"))
+				}
 				return
 			}
 			next.ServeHTTP(w, r)

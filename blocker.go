242f37ec728fbd0511bf5ceae83f20509b56ee24
jjcapellan <soycape@hotmail.com>
2022-02-18 19:11:57 +0100

    Rename map

--- blocker.go
+++ blocker.go
@@ -12,15 +12,15 @@ type loginAttemps struct {
 }
 
 // Stores failed logings: map[user+IP]loginAttemps
-var failedLoginStore map[string]loginAttemps = make(map[string]loginAttemps)
+var badLoginStore map[string]loginAttemps = make(map[string]loginAttemps)
 
 var mtxfailedLoginStore *sync.Mutex = &sync.Mutex{}
 
 func RegBadLogin(user string, ip string) {
 	key := user + strings.Split(ip, ":")[0]
-	userIpRegister, ok := failedLoginStore[key]
+	userIpRegister, ok := badLoginStore[key]
 	if !ok {
-		failedLoginStore[key] = loginAttemps{1, time.Now().Unix() + conf.banDuration}
+		badLoginStore[key] = loginAttemps{1, time.Now().Unix() + conf.banDuration}
 		return
 	}
 
@@ -30,20 +30,20 @@ func RegBadLogin(user string, ip string) {
 		expireTime = 0
 	}
 	attemps++
-	failedLoginStore[key] = loginAttemps{attemps, expireTime}
+	badLoginStore[key] = loginAttemps{attemps, expireTime}
 }
 
 func IsBlocked(user string, ip string) bool {
 	key := user + strings.Split(ip, ":")[0]
 
-	userIpRegister, ok := failedLoginStore[key]
+	userIpRegister, ok := badLoginStore[key]
 	if !ok {
 		return false
 	}
 
 	if userIpRegister.exp < time.Now().Unix() {
 		mtxfailedLoginStore.Lock()
-		delete(failedLoginStore, key)
+		delete(badLoginStore, key)
 		mtxfailedLoginStore.Unlock()
 		return false
 	}

364f58400c98b0b031c1fc0be788458ac52d72fb
jjcapellan <soycape@hotmail.com>
2022-02-18 19:16:10 +0100

    Renames

--- blocker.go
+++ blocker.go
@@ -6,45 +6,45 @@ import (
 	"time"
 )
 
-type loginAttemps struct {
+type userLogins struct {
 	attemps int   // Number of failed attemps
 	exp     int64 // Banned expire time
 }
 
 // Stores failed logings: map[user+IP]loginAttemps
-var badLoginStore map[string]loginAttemps = make(map[string]loginAttemps)
+var badLoginStore map[string]userLogins = make(map[string]userLogins)
 
-var mtxfailedLoginStore *sync.Mutex = &sync.Mutex{}
+var mtxBadLoginStore *sync.Mutex = &sync.Mutex{}
 
 func RegBadLogin(user string, ip string) {
 	key := user + strings.Split(ip, ":")[0]
-	userIpRegister, ok := badLoginStore[key]
+	obj, ok := badLoginStore[key]
 	if !ok {
-		badLoginStore[key] = loginAttemps{1, time.Now().Unix() + conf.banDuration}
+		badLoginStore[key] = userLogins{1, time.Now().Unix() + conf.banDuration}
 		return
 	}
 
-	attemps := userIpRegister.attemps
+	attemps := obj.attemps
 	expireTime := time.Now().Unix() + conf.banDuration
 	if attemps < conf.maxAttemps {
 		expireTime = 0
 	}
 	attemps++
-	badLoginStore[key] = loginAttemps{attemps, expireTime}
+	badLoginStore[key] = userLogins{attemps, expireTime}
 }
 
 func IsBlocked(user string, ip string) bool {
 	key := user + strings.Split(ip, ":")[0]
 
-	userIpRegister, ok := badLoginStore[key]
+	obj, ok := badLoginStore[key]
 	if !ok {
 		return false
 	}
 
-	if userIpRegister.exp < time.Now().Unix() {
-		mtxfailedLoginStore.Lock()
+	if obj.exp < time.Now().Unix() {
+		mtxBadLoginStore.Lock()
 		delete(badLoginStore, key)
-		mtxfailedLoginStore.Unlock()
+		mtxBadLoginStore.Unlock()
 		return false
 	}
 
